496a7f59a95af06f5b9ca9d9000598b001e9905f
Abdelhaq-Marouan <abdelhaqmarouan70@gmail.com>
2025-04-20 22:13:59 +0100

    ps

--- cmd.c
+++ cmd.c
@@ -6,17 +6,40 @@
 #include <string.h>
 #include "minishell.h"
 
-int check_path(char *input, char **cp_env)
+int check_ext(char *input, char **cp_env)
 {
     int i = 0;
     int j = 0;
     char *path = get_env_var(cp_env, "PATH");
     char **path_split = ft_split(path, ':');
-    
-    
+    while (path_split[i])
+    {
+        char *cmd = ft_strjoin(path_split[i], "/");
+        cmd = ft_strjoin(cmd, input);
+        if (access(cmd, F_OK) == 0)
+        {
+            free(path);
+            free(path_split);
+            return (1);
+        }
+        free(cmd);
+        i++;
+    }
+    free(path);
+    free(path_split);
+    return (0);
 }
-int check_cmd(char *input, char **cp_env)
+int check_in(char *input, char **cp_env)
 {
     char *cmd[] = {"echo", "cd", "pwd", "export", "unset", "env", "exit"};
-    
+    int i = 0;
+    while (cmd[i])
+    {
+        if (ft_strncmp(input, cmd[i], ft_strlen(cmd[i])) == 0)
+        {
+            return (1);
+        }
+        i++;
+    }
+    return (0);
 }
