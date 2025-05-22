39885e5f35f6262adcfe667b014f7ac80b7ddabb
HajarEssaoudi <hajares818@gmail.com>
2025-05-22 17:12:50 +0100

    execute_executable

--- execute_cmd.c
+++ execute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hes-saou <hes-saou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:55:03 by hes-saou          #+#    #+#             */
-/*   Updated: 2025/05/05 00:57:41 by hes-saou         ###   ########.fr       */
+/*   Updated: 2025/05/22 17:12:28 by hes-saou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	execute_built_in(t_tok *tok)
 		execute_exit(tok);
 }
 
-void	execute_simple_cmd(t_tok *tok, char **env)
+void	 execute_simple_cmd(t_tok *tok, char **env)
 {
 	char	**args;
 	pid_t	pid;
@@ -35,10 +35,31 @@ void	execute_simple_cmd(t_tok *tok, char **env)
 	pid = fork();
 	if (pid == 0)
 	{
-		// printf("Path: %s\n", tok->path);
-		// for (int i = 0; tok->str[i]; ++i)
-		// 	printf("Arg[%d]: %s\n", i, tok->str[i]);
 		if (execve(tok->path, tok->str, env) == -1)
+		{
+			perror("minishell failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid > 0)
+		waitpid(pid, &status, 0);
+	else
+	{
+		perror("fork failed");
+		return ;
+	}
+}
+
+void	 execute_executable(t_tok *tok, char **env)
+{
+	char	**args;
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(tok->execute, tok->str, env) == -1)
 		{
 			perror("minishell failed");
 			exit(EXIT_FAILURE);
@@ -55,7 +76,9 @@ void	execute_simple_cmd(t_tok *tok, char **env)
 
 void	execute_cmd(t_tok *tok, char **env)
 {
-	if(is_built_in(tok->str[0], env))
+	if (tok->execute)
+		execute_executable(tok, env);
+	else if(is_built_in(tok->str[0], env))
 		execute_built_in(tok);
 	else
 		execute_simple_cmd(tok, env);
