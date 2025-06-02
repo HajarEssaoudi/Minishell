db79c3ae9fa18c1bbda6ea2ccd0896e1fe7efe03
HajarEssaoudi <hajares818@gmail.com>
2025-06-02 15:17:33 +0100

    implement env into  a linked list

--- execute_env.c
+++ execute_env.c
@@ -6,53 +6,59 @@
 /*   By: hes-saou <hes-saou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 11:19:31 by hes-saou          #+#    #+#             */
-/*   Updated: 2025/06/02 11:46:43 by hes-saou         ###   ########.fr       */
+/*   Updated: 2025/06/02 15:16:14 by hes-saou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_env *init_lst_env(t_env *env, char **arr_env)
+t_env *init_node_env(char *str_env)
 {
-	int	i;
+	t_env	*node_env;
+	char	*equal;
 
-	i = 0;
-	while(arr_env[i])
+	node_env = malloc(sizeof(t_env));
+	if(!node_env)
 	{
-		env->key = NULL;
-		env->value = ft_strrchr(arr_env[i], '=') + 1;
-		i++;
+		ft_putstr_fd("allocation failed\n", 2);
+		//clear_exit;
 	}
-	return (env);
-}
-
-void print_list(t_env *env)
-{
-	int i;
-	t_env *tmp;
-
-	i = 0;
-	tmp = env;
-	while (tmp)
+	equal = ft_strchr(str_env, '=');
+	if (equal)
 	{
-		printf("value == %s\n", tmp->value);
-		tmp = tmp->next;
+		node_env->key = ft_substr(str_env, 0, (equal - str_env + 1));
+		node_env->value = ft_strdup(equal + 1);
 	}
+	else
+	{
+		return (NULL);
+	}
+	return (node_env);
 }
 
 t_env *create_list_env(char **arr_env)
 {
-	t_env *lst_env;
+	t_env	*head;
+	t_env	*tmp;
+	int		i;
 
-	lst_env = malloc(sizeof(t_env));
-	if (!lst_env)
+	i = 0;
+	head = NULL;
+	tmp = NULL;
+	while(arr_env[i])
 	{
-		//ft_clear_exit
-		ft_putstr_fd("allocation failed\n", 2);
+		t_env *new_node = init_node_env(arr_env[i]);
+		if (new_node == NULL)
+		{
+			ft_putstr_fd("allocation failed\n", 2);
+			//clear_exit
+		}
+		if (!head)
+			head = new_node;
+		else
+			tmp->next = new_node;
+		tmp = new_node;
+		i++;
 	}
-	lst_env->key = NULL;
-	lst_env->value = NULL;
-	lst_env = init_lst_env(lst_env, arr_env);
-	print_list(lst_env);
-	return(lst_env);
+	return (head);
 }
